2305095f2c26aecbab956ef8cfa30b3a8cac73be
AlexanderYukhanov <a.yukhanov@gmail.com>
2022-02-01 21:14:21 -0800

    Day 4, part 2

---
+++ day4_2/src/main.rs
@@ -0,0 +1,100 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+    usize,
+};
+
+#[derive(Debug)]
+struct Board {
+    values: [[i32; 5]; 5],
+    closed: bool,
+}
+
+impl Board {
+    fn read_from<T: BufRead>(input: &mut T) -> Result<Board, Error> {
+        let mut result = Board {
+            values: [[0; 5]; 5],
+            closed: false,
+        };
+        for i in 0..5 {
+            let mut line = String::new();
+            input.read_line(&mut line)?;
+            for (j, v) in line.split_whitespace().enumerate() {
+                result.values[i][j] = v.parse().unwrap();
+            }
+        }
+        Ok(result)
+    }
+
+    fn select(&mut self, value: i32) -> bool {
+        if self.closed {
+            return false;
+        }
+        for i in 0..5 {
+            for j in 0..5 {
+                if self.values[i][j] == value {
+                    self.values[i][j] = -1;
+                    if self.winner(i, j) {
+                        self.closed = true
+                    }
+                    break;
+                }
+            }
+        }
+        self.closed
+    }
+
+    fn winner(&self, i: usize, j: usize) -> bool {
+        let mut good_column = true;
+        let mut good_row = true;
+        for k in 0..5 {
+            if self.values[k][j] != -1 {
+                good_column = false;
+            }
+            if self.values[i][k] != -1 {
+                good_row = false;
+            }
+        }
+        good_column || good_row
+    }
+
+    fn value(&self) -> i32 {
+        let mut result = 0;
+        for i in 0..5 {
+            for j in 0..5 {
+                if self.values[i][j] != -1 {
+                    result += self.values[i][j];
+                }
+            }
+        }
+        result
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let mut input = BufReader::new(File::open("input")?);
+    let mut str_selected: String = String::new();
+    input.read_line(&mut str_selected).unwrap();
+    let selected = str_selected
+        .trim()
+        .split(',')
+        .map(|l| l.parse::<i32>().unwrap());
+
+    let mut boards: Vec<Board> = vec![];
+    let mut skip = String::new();
+    while input.read_line(&mut skip).unwrap() > 0 {
+        boards.push(Board::read_from(&mut input)?);
+    }
+
+    let mut last = 0;
+    for v in selected {
+        for board in &mut boards {
+            if board.select(v) {
+                last = v * board.value();
+            }
+        }
+    }
+
+    println!("Result: {}", last);
+    Ok(())
+}
