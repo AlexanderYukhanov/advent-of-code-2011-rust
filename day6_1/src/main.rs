dfb99036a19a8dd62c244b8b8997320f59cb730b
AlexanderYukhanov <a.yukhanov@gmail.com>
2022-02-08 08:31:13 -0800

    Day 6, part 1

---
+++ day6_1/src/main.rs
@@ -0,0 +1,30 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+    usize,
+};
+
+fn main() -> Result<(), Error> {
+    let mut input = BufReader::new(File::open("input")?);
+    let mut input_str = String::new();
+    input.read_line(&mut input_str).unwrap();
+
+    let mut prev = [1; 9];
+    for _ in 0..80 {
+        let mut cur = [0; 9];
+        cur[0] = prev[6] + prev[8];
+        for i in 1..9 {
+            cur[i] = prev[i - 1];
+        }
+        prev = cur;
+    }
+
+    let result: i32 = input_str
+        .trim()
+        .split(",")
+        .map(|v| v.parse::<usize>().unwrap())
+        .fold(0_i32, |v, i| v + prev[i]);
+
+    println!("Result: {}", result);
+    Ok(())
+}
