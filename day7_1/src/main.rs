6bb402fdd678edca5b1c9990521af4d10e841315
AlexanderYukhanov <a.yukhanov@gmail.com>
2022-02-09 01:19:55 -0800

    Removed dead code

--- day7_1/src/main.rs
+++ day7_1/src/main.rs
@@ -8,16 +8,6 @@ fn main() -> Result<(), Error> {
     let mut input_str = String::new();
     input.read_line(&mut input_str).unwrap();
 
-    let mut prev = [1; 9];
-    for _ in 0..80 {
-        let mut cur = [0; 9];
-        cur[0] = prev[6] + prev[8];
-        for i in 1..9 {
-            cur[i] = prev[i - 1];
-        }
-        prev = cur;
-    }
-
     let mut positions: Vec<i32> = input_str
         .trim()
         .split(",")
