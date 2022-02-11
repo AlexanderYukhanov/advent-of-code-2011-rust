4ff47321c1150b21e4837c1ea63c22bee6b8ca84
AlexanderYukhanov <a.yukhanov@gmail.com>
2022-02-10 19:43:10 -0800

    Day 8, part 1

---
+++ day8_1/src/main.rs
@@ -0,0 +1,20 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error},
+};
+
+fn main() -> Result<(), Error> {
+    let input = BufReader::new(File::open("input")?);
+    let result: u32 = input
+        .lines()
+        .map(|v| v.unwrap())
+        .map(|v| v.split('|').nth(1).expect("").to_string())
+        .map(|v| v.split(" ").map(|v| match v.len() {
+                2 | 3 | 4 | 7 => 1,
+                _ => 0,
+            }).sum::<u32>())
+        .sum();
+
+    println!("Result: {}", result);
+    Ok(())
+}
